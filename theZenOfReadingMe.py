a3fb7ec85013fb1df157a30684e60a90a388b317
Abdullah <ab-benkathir@outlook.com>
2022-04-04 12:03:03 +0200

    opdracht 1

---
+++ theZenOfReadingMe.py
@@ -0,0 +1,12 @@
+import time
+
+ 
+
+with open('zentext.txt' , 'r' ) as f:
+    zen_lines = f.readline()
+    while zen_lines:
+        time.sleep(1)
+        zen_lines = f.readline()
+        print(zen_lines )
+
+
